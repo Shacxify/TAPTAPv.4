dab2b232e000799271aaceaf27fe644e965f9f3c
Cash Johnson <me@cashjohnson.net>
2016-12-25 14:15:40 -0800

    ALL FILES IMPORT

---
+++ Assets/playClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playClick : MonoBehaviour {
+
+
+	public string welcome = "It worked!";
+	// Use this for initialization
+	public void onClick () {
+		Debug.Log(welcome);
+	}
+}
