70f5df7b89bb080ed952ac06479a60d04db1e3e6
Cash Johnson <me@cashjohnson.net>
2017-04-06 21:20:45 -0700

    v 4.1
    Jump up redesign bitches

--- Assets/Scripts/readyUp.cs
+++ Assets/Scripts/readyUp.cs
@@ -10,7 +10,7 @@ public class readyUp : MonoBehaviour {
 	public Animator anim;
 
 	public void Update () {
-		anim = GameObject.Find("Canvas").GetComponent<Animator>();
+		//anim = GameObject.Find("Canvas").GetComponent<Animator>();
 		readyw1 = GameObject.Find("fingerPrints/wPrintL").GetComponent<askReadyUp>().ready;
 		readyw2 = GameObject.Find("fingerPrints/wPrintR").GetComponent<askReadyUp>().ready;
 		readyb1 = GameObject.Find("fingerPrints/bPrintL").GetComponent<askReadyUp>().ready;
@@ -20,18 +20,19 @@ public class readyUp : MonoBehaviour {
 
 		if (readyw1 == true && readyw2 == true && readyb1 == true && readyb2 == true) {
 			anim.SetBool("ready", true);
-			//counterAnim.SetTrigger("allIn");
 			if (anim.GetCurrentAnimatorStateInfo(0).IsName("outReadyUp") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) {
 				if (GameObject.Find("/constant") != null) {
 					if (GameObject.Find("/constant").GetComponent<gameMode>().game == 1) {
 						Application.LoadLevel("TAPTAP");
-						} else if (GameObject.Find("/constant").GetComponent<gameMode>().game == 0 || GameObject.Find("/constant").GetComponent<gameMode>().game == null) {
+					} else if (GameObject.Find("/constant").GetComponent<gameMode>().game == 2) {
+						Application.LoadLevel("TOW");
+					} else if (GameObject.Find("/constant").GetComponent<gameMode>().game == 0 || GameObject.Find("/constant").GetComponent<gameMode>().game == null) {
 							Application.LoadLevel("TAPTAP");
-						}
-					} else {
-						Application.LoadLevel("TAPTAP");
 					}
+				} else {
+						Application.LoadLevel("TAPTAP");
 				}
 			}
 		}
 	}
+}
